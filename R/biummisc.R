6f32abf3acbd72d09261e7add35f1b2ea0ed8d09
Federico Marini <marinif@uni-mainz.de>
2018-09-19 11:39:37 +0200

    added pkg doc for the pkg itself

---
+++ R/biummisc.R
@@ -0,0 +1,11 @@
+#' BIUMmisc
+#'
+#' This is a collection of functions and scripts commonly used by the
+#' BIUM-MZ Core Facility
+#'
+#' @author Federico Marini
+#'
+#' @name BIUMmisc
+#' @docType package
+NULL
+
