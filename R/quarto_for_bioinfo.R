af1f2aa5c4b7f5f8fe2cc724a70864a0eb066b09
Federico Marini <marinif@uni-mainz.de>
2025-07-22 16:02:26 +0200

    probably a better way of handling the creation of all things, with file.path and a bit more customizable paths themselves

--- R/quarto_for_bioinfo.R
+++ R/quarto_for_bioinfo.R
@@ -1,50 +1,86 @@
+#' Title
+#'
+#' @param file_name
+#' @param report_folder
+#' @param ext_name
+#'
+#' @returns
+#' @export
+#'
+#' @importFrom cli cli_alert_info cli_alert_success cli_alert_warning
+#'
+#' @examples
 create_bioinfo_html <- function(file_name = NULL,
-                                is_vignette = FALSE,
-                                ext_name = "bioinfo-html") {
+                                report_folder = NULL,
+                                ext_name = "bioinfo-html",
+                                expose_aux_files = TRUE) {
 
   if (is.null(file_name)) {
     stop("You must provide a valid file_name")
+  }
+
+  if (is.null(report_folder)) {
+    cli::cli_alert_info("Defaulting to create the report and its associated files in the current directory")
+    report_folder <- "."
+  } else {
+    if(!dir.exists(report_folder)) {
+      dir.create(report_folder)
+      cli::cli_alert_info("Created report folder into ", report_folder)
+    }
   }
 
   # check for available extensions
   stopifnot("Extension not in package" = ext_name %in% c("bioinfo-html"))
 
   # check for existing _extensions directory
-  if(!file.exists("_extensions")) dir.create("_extensions")
-  message("Created '_extensions' folder")
+  if(!dir.exists(file.path(report_folder, "_extensions"))) {
+    dir.create(file.path(report_folder, "_extensions"))
+    cli::cli_alert_info("Created '_extensions' folder into ", report_folder)
+  }
 
-  # create folder
-  ## TODO: possibly cover the fact it is going to be a vignette?
-  if(!file.exists(paste0("_extensions/", ext_name))) dir.create(paste0("_extensions/", ext_name))
+  # create folder for the specific extension
+  if(!dir.exists(file.path(report_folder, "_extensions/", ext_name)))
+    dir.create(file.path(report_folder, "_extensions/", ext_name))
 
   # copy from internals
   file.copy(
-    from = system.file(paste0("extdata/_extensions/", ext_name), package = "BIUMmisc"),
-    to = paste0("_extensions/"),
+    from = system.file("extdata", "_extensions/", ext_name, package = "BIUMmisc"),
+    to = file.path(report_folder, "_extensions/"),
     overwrite = TRUE,
     recursive = TRUE,
     copy.mode = TRUE
   )
 
   # logic check to make sure extension files were moved
-  n_files <- length(dir(paste0("_extensions/", ext_name)))
+  n_files <- length(list.files(file.path(report_folder, "_extensions/", ext_name)))
 
   if(n_files >= 3){
-    message(paste(ext_name, "was installed to _extensions folder in current working directory."))
+    cli::cli_alert_info(
+      paste(ext_name, "was installed to _extensions folder in the specified report directory.")
+    )
   } else {
-    message("Extension appears not to have been created")
+    cli::cli_alert_warning("Extension appears not to have been created")
   }
 
   # create new qmd report based on skeleton
-  file.copy("_extensions/bioinfo-html/bioinfo-html-report-template.qmd",
-            paste0(file_name, ".qmd", collapse = ""))
+  file.copy(file.path(report_folder, "_extensions", "bioinfo-html", "bioinfo-html-report-template.qmd"),
+            file.path(report_folder, paste0(file_name, ".qmd")))
+
+  if (expose_aux_files) {
+    file.copy(file.path(report_folder, "_extensions", "bioinfo-html", "references.bib"),
+              file.path(report_folder, "references.bib"))
+    file.copy(file.path(report_folder, "_extensions", "bioinfo-html", "ummz_theme.scss"),
+              file.path(report_folder, "ummz_theme.scss"))
+    cli::cli_alert_info("Copied auxiliary files into the toplevel report folder",
+                        "You might want to edit the corresponding lines in the yaml section")
+  }
+
 
   # open the new file in the editor
-  file.edit(paste0(file_name, ".qmd", collapse = ""))
+  file.edit(file.path(report_folder, paste0(file_name, ".qmd")))
 
-  # TODO: some better messages all around, maybe?
 
-  cli::cli_alert_success("Have fun with your report!")
+  cli::cli_alert_success("Have fun with your report - available inside the folder", report_folder)
 }
 
 
